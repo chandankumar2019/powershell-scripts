9191adfca0893edf8f4f7bd08fac175d3b533307
CHANDAN KUMAR PRADHAN <168193729+chandankumar2019@users.noreply.github.com>
2025-04-07 17:57:39 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,15 @@
-Write-Host "Hello World"
-Write-Host "This is a Test" -NoNewline
+#Write-Host "Hello World"
+#Write-Host "This is a Test" -NoNewline
+
+# install.ps1
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install VS Code
+choco install vscode -y
+
+# Install Notepad++
+choco install notepadplusplus -y
